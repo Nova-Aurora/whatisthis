284ace4d0b6a3a12047e3eebada203fb2af5787f
Nova-Aurora <62871042+Nova-Aurora@users.noreply.github.com>
2022-07-06 03:09:20 -0500

    whatisthis

--- canvas.js
+++ canvas.js
@@ -18,13 +18,16 @@ async function getRenderingContext(canvasElement, imagesrc) {
         ctx: ctx,
         ox: 0,
         oy: 0,
-        dragging: false
+        zoom: 1,
+        dragging: false,
+        lastX: 0,
+        lastY: 0,
     }
 }
 function newpaint(canvas) {
     const matrix = new DOMMatrix;
     const pattern = canvas.ctx.createPattern(canvas.img, 'repeat');
-    pattern.setTransform(matrix.translateSelf(canvas.ox, canvas.oy));
+    pattern.setTransform(matrix.translateSelf(canvas.ox, canvas.oy).scaleSelf(canvas.zoom));
     canvas.ctx.fillStyle = pattern;
     canvas.ctx.fillRect(0, 0, canvas.ctx.canvas.width, canvas.ctx.canvas.height);
 
@@ -39,10 +42,10 @@ export async function startCanvas(canvasElement, imagesrc) {
     }
     resize();
     window.onresize = resize;
-    window.onmousedown = () => {canvas.dragging = true};
-    window.ontouchstart = () => {canvas.dragging = true};
-    window.onmouseup = () => {canvas.dragging = false};
-    window.ontouchend = () => {canvas.dragging = false};
+    window.onmousedown = window.ontouchstart = () => {canvas.dragging = true};
+    //window.ontouchstart = () => {canvas.dragging = true};
+    window.onmouseup = window.ontouchend = () => {canvas.dragging = false};
+    //window.ontouchend = () => {canvas.dragging = false};
     window.onmousemove = event => {
         if (!canvas.dragging) {return}
         canvas.ox = (((canvas.ox + event.movementX) / canvas.ctx.canvas.width) % 1) * canvas.ctx.canvas.width;
@@ -51,8 +54,10 @@ export async function startCanvas(canvasElement, imagesrc) {
     }
     window.ontouchmove = event => {
         if (!canvas.dragging) {return}
-        canvas.ox = (((canvas.ox + event.movementX) / canvas.ctx.canvas.width) % 1) * canvas.ctx.canvas.width;
-        canvas.oy = ((((canvas.oy + event.movementY) / canvas.ctx.canvas.height) % 1) * canvas.ctx.canvas.height);
+        const movementX = event.changedTouches[0].clientX - canvas.lastX;
+        const movementY = event.changedTouches[0].clientY - canvas.lastY;
+        canvas.ox = (((canvas.ox + movementX) / canvas.ctx.canvas.width) % 1) * canvas.ctx.canvas.width;
+        canvas.oy = ((((canvas.oy + movementY) / canvas.ctx.canvas.height) % 1) * canvas.ctx.canvas.height);
         newpaint(canvas);
     }
 }

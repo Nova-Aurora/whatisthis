96bbb588262625b15d924312249cca4a7fae6b29
Nova-Aurora <62871042+Nova-Aurora@users.noreply.github.com>
2022-07-06 02:29:21 -0500

    whatisthis

--- canvas.js
+++ canvas.js
@@ -40,8 +40,16 @@ export async function startCanvas(canvasElement, imagesrc) {
     resize();
     window.onresize = resize;
     window.onmousedown = () => {canvas.dragging = true};
+    window.ontouchstart = () => {canvas.dragging = true};
     window.onmouseup = () => {canvas.dragging = false};
+    window.ontouchend = () => {canvas.dragging = false};
     window.onmousemove = event => {
+        if (!canvas.dragging) {return}
+        canvas.ox = (((canvas.ox + event.movementX) / canvas.ctx.canvas.width) % 1) * canvas.ctx.canvas.width;
+        canvas.oy = ((((canvas.oy + event.movementY) / canvas.ctx.canvas.height) % 1) * canvas.ctx.canvas.height);
+        newpaint(canvas);
+    }
+    window.ontouchmove = event => {
         if (!canvas.dragging) {return}
         canvas.ox = (((canvas.ox + event.movementX) / canvas.ctx.canvas.width) % 1) * canvas.ctx.canvas.width;
         canvas.oy = ((((canvas.oy + event.movementY) / canvas.ctx.canvas.height) % 1) * canvas.ctx.canvas.height);
--- whatisthis.js
+++ whatisthis.js
@@ -1,7 +1,4 @@
 import { startCanvas } from "./canvas.js";
 const whatisthis = document.getElementById('whatisthis');
 whatisthis.hidden = false;
-/*autoResize(whatisthis);
-autoPaint(whatisthis, './whatisthis.png');
-*/
 startCanvas(whatisthis, './whatisthis.png');
